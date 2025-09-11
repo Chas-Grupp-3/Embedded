7c80d4ba18c7caa523a25a875e93b05a6a66f178
VSilkis <victor.silkisberg@chasacademy.se>
2025-09-11 14:20:57 +0200

    Added basic temperature thresholds and comments

--- package_sensor/include/DHTSensor.h
+++ package_sensor/include/DHTSensor.h
@@ -4,11 +4,47 @@
 #include <Arduino.h>
 #include <DHT.h>
 
+struct threshold //We will get these later from app
+    {
+        float minTemp = 20.0;
+        float maxTemp = 28.0;   
+    };
+
+    enum class Status //Status for temperature
+    {
+        NORMAL,
+        HIGH_TEMP,
+        LOW_TEMP,
+        ERROR
+    };
+
+
+Status checkStatus(float temperature,const threshold& limits)
+    {
+        if (isnan(temperature)) // Check if reading was successful
+        {
+            return Status::ERROR;
+        }
+        else if (temperature < limits.minTemp) // Check if below minTemp
+        {
+            return Status::LOW_TEMP;
+        }
+        else if (temperature > limits.maxTemp) // Check if above maxTemp
+        {
+            return Status::HIGH_TEMP;
+        }
+        else
+        {
+            return Status::NORMAL; // Check if we are inbetween min - max
+        
+        }
+    }
+
 namespace DHTSensor
 {
 
-    uint8_t DHT_PIN = 5; //Change for the actual pin used 
-    const uint8_t DHT_TYPE = DHT11;
+    uint8_t DHT_PIN = 5; //Change to appropriate pin
+    const uint8_t DHT_TYPE = DHT11; //DHT11 sensor. change to DHT22 if needed
     DHT dht(DHT_PIN, DHT_TYPE);
     float temperature;
     float humidity;
@@ -18,24 +54,20 @@ namespace DHTSensor
     void readDHTSensor();
     void printDHTSensor();
     
-    void initDHTSensor(uint8_t pin)
-    {
-        DHTSensor::DHT_PIN = pin;
-        dht.begin();
-    }
+    
 
-    void initDHTSensor()
+    void initDHTSensor() //Start the sensor
     {
         dht.begin();
     }
 
-    void readDHTSensor()
+    void readDHTSensor() //Save the readings to variables
     {
-        temperature = dht.readTemperature();
         humidity = dht.readHumidity();
+        temperature = dht.readTemperature();
     }
 
-    void printDHTSensor()
+    void printDHTSensor() //print if needed, we are checking temp in src
     {
         Serial.print("Temperature: ");
         Serial.print(temperature, 1);
@@ -44,7 +76,7 @@ namespace DHTSensor
         Serial.print(humidity);
         Serial.println("%");
     }
-
+    
 }
 
 #endif
--- package_sensor/src/main.cpp
+++ package_sensor/src/main.cpp
@@ -1,18 +1,65 @@
 #include <Arduino.h>
 #include <DHTSensor.h>
 
+threshold limits;
+Status currentStatus = Status::NORMAL;
+
+//Readings timing, we later set these for longer durations
+unsigned long lastReading = 0;
+unsigned long normalInterval = 10000;
+unsigned long errorInterval = 2000;
+unsigned long currentInterval = normalInterval;
 
 
 void setup() 
 {
   Serial.begin(9600);
-  DHTSensor::initDHTSensor(5);
+  DHTSensor::initDHTSensor();
   delay(2000);
 }
 
 void loop() {
-  DHTSensor::readDHTSensor();
-  DHTSensor::printDHTSensor();
-  delay(5000);
+  unsigned long now = millis(); // Get the current time
+  
+  if (now - lastReading >= currentInterval) // Time to take a new reading
+  {
+    lastReading = now;
+   
+    DHTSensor::readDHTSensor();
+    float temperature = DHTSensor::temperature;
+    currentStatus = checkStatus(temperature, limits);
+
+  if (currentStatus == Status::LOW_TEMP) //Too low temperature
+    {
+      Serial.println("Temperature is too low!");
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+       currentInterval= errorInterval;
+    }
+  
+  else if (currentStatus == Status::HIGH_TEMP) //Too high temperature
+  {
+    Serial.println("Temperature is too high!");
+    Serial.println("Temperature: ");
+    Serial.print(temperature);
+    currentInterval= errorInterval;
+  }
+  
+  else if (currentStatus == Status::ERROR) //Error reading from sensor (NaN)
+  {
+    Serial.println("\nError reading from DHT sensor!");
+     currentInterval= errorInterval;
+  }
+  
+  else //Within limits, normal temperature
+  {
+    Serial.println("\nTemperature is normal."); 
+    Serial.println("Temperature: ");
+    Serial.print(temperature);
+    currentInterval = normalInterval;
+  }
+  
+  }
+
 }
 
